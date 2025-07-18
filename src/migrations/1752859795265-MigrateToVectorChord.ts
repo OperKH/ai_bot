bc8afae8b54e4e59dc9a6fe337184b4e0b84c549
Aleksandr Zhytnyk <oper.kh@gmail.com>
2025-07-18 21:40:04 +0300

    Migrate to VectorChord

---
+++ src/migrations/1752859795265-MigrateToVectorChord.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrateToVectorChord1752859795265 implements MigrationInterface {
+    name = 'MigrateToVectorChord1752859795265'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS vchord;`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "chat_photo_message_embedding_idx"`);
+        await queryRunner.query(`ALTER TABLE "chat_photo_message" ALTER COLUMN "embedding" SET DATA TYPE real[]`);
+        await queryRunner.query(`ALTER TABLE "chat_photo_message" ALTER COLUMN "embedding" SET DATA TYPE vector(512)`);
+        await queryRunner.query(`
+            CREATE INDEX IF NOT EXISTS "chat_photo_message_embedding_idx" ON "chat_photo_message" USING vchordrq ("embedding" vector_l2_ops) WITH (options = $$
+                residual_quantization = true
+                [build.internal]
+                lists = []
+                spherical_centroids = false
+                build_threads = 4
+                sampling_factor = 1024
+            $$)`);
+        await queryRunner.query(`DROP EXTENSION IF EXISTS vectors`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS vectors;`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "chat_photo_message_embedding_idx"`);
+        await queryRunner.query(`ALTER TABLE "chat_photo_message" ALTER COLUMN "embedding" SET DATA TYPE real[]`);
+        await queryRunner.query(`ALTER TABLE "chat_photo_message" ALTER COLUMN "embedding" SET DATA TYPE vectors.vector(512)`);
+        await queryRunner.query(`CREATE INDEX "chat_photo_message_embedding_idx" ON "chat_photo_message" USING vectors ("embedding" vector_l2_ops)`);
+        await queryRunner.query(`DROP EXTENSION IF EXISTS vchord`);
+    }
+
+}
